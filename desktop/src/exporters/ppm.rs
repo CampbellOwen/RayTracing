263e0eb4e79d67ed24b30b25b41c6067efef9186
Owen Campbell <ocampbell95@gmail.com>
2021-07-27 21:36:32 -0700

    Gamma correction

--- desktop/src/exporters/ppm.rs
+++ desktop/src/exporters/ppm.rs
@@ -4,7 +4,10 @@ use std::{fs::File, io::Write};
 use renderer::Image;
 
 pub fn write_image(img: &Image, location: &str) -> Result<(), io::Error> {
-    assert_eq!(img.size.0 as usize * img.size.1 as usize * 3, img.data.len());
+    assert_eq!(
+        img.size.0 as usize * img.size.1 as usize * 3,
+        img.data.len()
+    );
     let mut file = File::create(&location)?;
     write!(file, "P3\n{} {}\n255\n", img.size.0, img.size.1)?;
 
@@ -24,9 +27,9 @@ pub fn write_image(img: &Image, location: &str) -> Result<(), io::Error> {
                     ))
                 }
             };
-            let r = (pixel.x * 255.999) as u32;
-            let g = (pixel.y * 255.999) as u32;
-            let b = (pixel.z * 255.999) as u32;
+            let r = (pixel.x.sqrt().clamp(0.0, 0.999) * 256.0) as u32;
+            let g = (pixel.y.sqrt().clamp(0.0, 0.999) * 256.0) as u32;
+            let b = (pixel.z.sqrt().clamp(0.0, 0.999) * 256.0) as u32;
 
             write!(file, "{} {} {}\n", r, g, b)?;
         }
