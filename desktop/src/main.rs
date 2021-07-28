aec3259ddc6556dab9509e880ffaefc29372c061
Owen Campbell <ocampbell95@gmail.com>
2021-07-27 18:15:47 -0700

    Use camera

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,5 +1,5 @@
 use exporters::ppm::write_image;
-use renderer::{ray_colour, Hittable, Image, Ray, Sphere, Vec3};
+use renderer::{ray_colour, Camera, Hittable, Image, Ray, Sphere, Vec3};
 
 mod exporters;
 
@@ -13,28 +13,6 @@ fn main() {
     // Camera
     let viewport_height = 2.0;
     let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Vec3::origin();
-    let horizontal = Vec3 {
-        x: viewport_width,
-        y: 0.0,
-        z: 0.0,
-    };
-    let vertical = Vec3 {
-        x: 0.0,
-        y: viewport_height,
-        z: 0.0,
-    };
-
-    let lower_left_corner = origin
-        - horizontal / 2.0
-        - vertical / 2.0
-        - Vec3 {
-            x: 0.0,
-            y: 0.0,
-            z: focal_length,
-        };
 
     let objects: Vec<&dyn Hittable> = vec![
         &Sphere {
@@ -63,18 +41,14 @@ fn main() {
         },
     ];
 
+    let camera = Camera::new();
+
     for y in 0..img.size.1 {
         for x in 0..img.size.0 {
-            //img.data.push((x as f32) / (img.size.0 - 1) as f32);
-            //img.data.push((y as f32) / (img.size.1 - 1) as f32);
-            //img.data.push(0.25);
-
             let u = x as f32 / (width - 1) as f32;
             let v = y as f32 / (height - 1) as f32;
-            let ray = Ray {
-                origin: origin,
-                dir: lower_left_corner + (horizontal * u) + (vertical * v) - origin,
-            };
+
+            let ray = camera.get_ray(u, v);
             let colour = ray_colour(&ray, &objects);
             img.put(x, y, &colour);
         }
